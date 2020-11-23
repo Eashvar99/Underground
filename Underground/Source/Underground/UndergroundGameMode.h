33ebf310ec4314533e87f5d93a68e307340feb81
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2020-11-23 14:01:29 -0500

    Project Files

---
+++ Underground/Source/Underground/UndergroundGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UndergroundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUndergroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUndergroundGameMode();
+};
+
+
+
